77667963a83d5a63d9582923f61a625d09332d7c
hyun6ik <fkanwm@naver.com>
2021-01-07 22:44:39 +0900

    spring mvc

---
+++ src/test/java/springweb/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package springweb.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
