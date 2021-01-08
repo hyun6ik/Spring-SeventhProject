067aec43601af78fadffc974ad54d46b275203dd
hyun6ik <fkanwm@naver.com>
2021-01-08 23:00:25 +0900

    AppConfig 리펙토링

--- src/main/java/springweb/mvc/AppConfig.java
+++ src/main/java/springweb/mvc/AppConfig.java
@@ -1,5 +1,6 @@
 package springweb.mvc;
 
+import springweb.mvc.discount.DiscountPolicy;
 import springweb.mvc.discount.FixDiscountPolicy;
 import springweb.mvc.repository.MemoryMemberRepository;
 import springweb.mvc.service.MemberService;
@@ -10,13 +11,20 @@ import springweb.mvc.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 
 
 }
