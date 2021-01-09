69c51b2d3cae4e5f934f16b4dee9fb3b775f9e19
hyun6ik <fkanwm@naver.com>
2021-01-09 16:13:30 +0900

    컨테이너에서 등록된 모든 빈 조회 모든 빈 출력하기 - ac.getBeanDefinitionNames() : 스프링에 등록된 모든 빈의 이름을 조회한다. - ac.getBean() : 빈 이름으로 빈 객체를 조회한다.
    애플리케이션 빈 출력하기
    - 스프링이 내부에서 사용하는 빈은 getRole()로 구분할 수 있다.
    - ROLE_APPLICATION : 일반적으로 사용자가 정의한 빈
    - ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈

---
+++ src/test/java/springweb/mvc/beenfind/ApplicationContextInfoTest.java
@@ -0,0 +1,49 @@
+package springweb.mvc.beenfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springweb.mvc.AppConfig;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    public void findAllBean() throws Exception {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+        //when
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    public void findApplicationBean() throws Exception {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            //ROLE_INFRASTRUCTURE : 스프링 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+
+        }
+        //when
+
+        //then
+
+    }
+}
