e49d552659a139194bce6315f34fb5f2fc3acc30
hyun6ik <fkanwm@naver.com>
2021-01-10 23:28:39 +0900

    request 스코프 예제 만들기

---
+++ src/main/java/springweb/mvc/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package springweb.mvc.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import springweb.mvc.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
