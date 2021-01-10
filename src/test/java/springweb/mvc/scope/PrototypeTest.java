154cf24a43998218459ec19935aeb850e58e1825
hyun6ik <fkanwm@naver.com>
2021-01-10 22:15:51 +0900

    프로토타입 빈 - 스프링 컨테이너에 요청할 때 마다 새로 생성된다. - 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입 그리고 초기화까지만 관여한다. - 종료 메서드가 호출되지 않는다. - 그래서 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야한다. 종료메서드에 대한 호출도 클라이언트가 직접 해야 한다.

---
+++ src/test/java/springweb/mvc/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package springweb.mvc.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    public void prototypeBeanFind() throws Exception {
+        //given
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        //when
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        //then
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        ac.close();
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init(){
+            System.out.println("singletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("singletonBean.destroy");
+        }
+    }
+}
